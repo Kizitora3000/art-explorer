f33788fa3e01a6ed6dce444612bfd002fe02fd4e
Kizitora3000 <kutaku2000@gmail.com>
2024-08-09 12:05:22 +0900

    add: indexHandler and index.tmpl

--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // JSONレスポンスを返す関数
 // gin.Context: HTTPリクエスト/レスポンス を管理する構造体
-func helloWorldHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello World",
-	})
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 }
 
 func main() {
 	// ginのコアとなるEngineインスタンスを作成
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
 
 	// ルートエンドポイント"/"にGETリクエストを処理するハンドラーを登録
-	router.GET("/", index)
+	router.GET("/", indexHandler)
 
 	// http://localhost:8080 でサーバを立てる
 	router.Run()
