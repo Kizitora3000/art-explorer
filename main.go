4d44f4e7a4d27c26e50385b473edfdbfc6bc3dcc
Kizitora3000 <kutaku2000@gmail.com>
2024-08-11 09:39:06 +0900

    add: redirect URL in MiAuth step2

--- main.go
+++ main.go
@@ -8,10 +8,27 @@ import (
 	"github.com/google/uuid"
 )
 
+func getRootPath(c *gin.Context) string {
+	scheme := "http"
+
+	// 原因不明: Azure App Serviceにデプロイすると，c.Request.TLS == nil となり scheme := "http" 扱いになるので一度コメントアウト
+	/*
+		if c.Request.TLS != nil {
+			scheme = "https"
+		}
+	*/
+
+	host := c.Request.Host
+	return fmt.Sprintf("%s://%s", scheme, host)
+}
+
 // JSONレスポンスを返す関数
 // gin.Context: HTTPリクエスト/レスポンス を管理する構造体
 func indexHandler(c *gin.Context) {
 	// ---------- MiAuth ----------
+
+	// TODO: 他のホストでログインしているユーザもいるため，ホストはユーザが選択できるようにする
+	host := "misskey.io"
 
 	// ランダムなUUIDを生成
 	sessionID, err := uuid.NewRandom()
@@ -19,16 +36,20 @@ func indexHandler(c *gin.Context) {
 		panic(err)
 	}
 
-	// TODO: 他のホストでログインしているユーザもいるため，ホストはユーザが選択できるようにする
-	host := "misskey.io"
+	redirectUri := fmt.Sprintf("%s/redirect", getRootPath(c))
 
-	authorizationURL := fmt.Sprintf("https://%s/miauth/%s", host, sessionID)
+	authorizationURL := fmt.Sprintf("https://%s/miauth/%s?callback=%s", host, sessionID, redirectUri)
 
 	// HTMLテンプレートに渡す
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"authorization_url": authorizationURL,
 		"session_id":        sessionID, // for Debug
 	})
+}
+
+// 認証後のリダイレクト先
+func redirectHandler(c *gin.Context) {
+	c.String(http.StatusOK, "This is a redirect page.")
 }
 
 func main() {
@@ -40,6 +61,9 @@ func main() {
 
 	// ルートエンドポイント"/"にGETリクエストを処理するハンドラーを登録
 	r.GET("/", indexHandler)
+
+	// 認証後のリダイレクト先である/redirectエンドポイントを登録
+	r.GET("/redirect", redirectHandler)
 
 	// http://localhost:8080 でサーバを立てる
 	r.Run()
